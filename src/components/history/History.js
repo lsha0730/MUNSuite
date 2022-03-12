b6c3153a181095a777d788fc035518bd5dfcabf9
Lincoln <Lsha0730@gmail.com>
2022-03-12 02:25:17 -0800

    History (Basics, Nonfunctional filter)

--- src/components/history/History.js
+++ src/components/history/History.js
@@ -3,14 +3,18 @@ import "./History.scoped.css";
 import MockSubmissions from "../inbox/MockSubmissions.js";
 import StandardCard from "../inbox/card/StandardCard.js";
 import CustomCard from "../inbox/card/CustomCard.js";
+import { GoTriangleDown } from "react-icons/go";
+import { FaFilter } from "react-icons/fa";
 
 function History() {
     const [cardArr, setCardArr] = useState(MockSubmissions);
+    const [cardTitles, setCardTitles] = useState(cardArr.map(card => {return card.title}));
     const [cardArrRender, setCardArrRender] = useState([]);
     const [selection, setSelection] = useState(0);
     const [selectionRender, setSelectionRender] = useState();
 
     useEffect(() => {
+        setCardTitles(cardArr.map(card => {return card.title}))
         let renderArr = [];
 
         for (let i=0; i<cardArr.length; i++) {
@@ -56,9 +60,62 @@ function History() {
                 {selectionRender}
             </div>
             <div className="UI-right">
+                <div className="UI-topright">
+                    <div className="filter-group">
+                        <FaFilter size={20} className="filter-icon"/>
+                        <Dropdown options={["Passed", "Failed", "Starred"]}/>
+                    </div>
+                </div>
                 <div className="cardbar-deck-container">
                     {cardArrRender}
                 </div>
+            </div>
+        </div>
+    )
+}
+
+
+function Dropdown(props) {
+    const [value, setValue] = useState(); // Stores the index of the item in options
+    const [dropVisible, setDropVisible] = useState(false);
+    const [dropRender, setDropRender] = useState();
+
+    let optionRenders = [];
+    for (let i=0; i<props.options.length; i++) {
+        let option = props.options[i];
+        optionRenders.push(
+            <div className="dropdown-option-container" onClick={() => {
+                setDropVisible(!dropVisible);
+                setValue(i);
+                }}>
+                <div className="dropdown-text-container">
+                    <p className="nowrap">{option}</p>
+                </div>
+            </div>
+        )
+    }
+
+    useEffect(() => {
+        if (dropVisible) {
+            setDropRender(
+                <div className="dropdown-field">
+                    {optionRenders}
+                </div>
+            );
+        } else {
+            setDropRender();
+        }
+    }, [dropVisible])
+
+    return (
+        <div>
+            <div className={dropVisible? "dropdown-defocuser":"hidden"} onClick={() => setDropVisible(false)}></div>
+            <div className={dropVisible? "dropdown-bar super-z":"dropdown-bar"} onClick={() => setDropVisible(!dropVisible)}>
+                <div className="dropdown-text-container">
+                    <p className="nowrap">{props.options[value]}</p>
+                </div>
+                <GoTriangleDown size={10} className="dropdown-triangle"/>
+                {dropRender}
             </div>
         </div>
     )
