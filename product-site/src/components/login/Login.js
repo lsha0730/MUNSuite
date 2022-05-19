56b6acd536f7cb1edc2a7602de76cdcb3b75304a
Lincoln <Lsha0730@gmail.com>
2022-05-19 06:38:25 -0700

    Product Site (Resizing)

--- product-site/src/components/login/Login.js
+++ product-site/src/components/login/Login.js
@@ -4,26 +4,28 @@ import "../register/Register.scoped.css";
 
 function Login() {
     return (
-        <div className="register-container">
-            <p className="form-header">Log In</p>
-            <div className="form">
-                <div className="form-left">
-                    <div style={{display: "flex", flexDirection: "column"}}>
-                        <p className="input-label">Email</p>
-                        <input type="text" className="input-field"></input>
-                    </div>
-                    <div style={{display: "flex", flexDirection: "column"}}>
-                        <p className="input-label">Password</p>
-                        <input type="password" className="input-field"></input>
-                    </div>
-                    <p className="additional-text" style={{marginTop:"10px"}}>
-                        <Link to="/forgot" className="additional-link">Forgot Password?</Link>
-                    </p>
+        <div className="centering-container">
+            <div className="register-container">
+                <p className="form-header">Log In</p>
+                <div className="form">
+                    <div className="form-left">
+                        <div style={{display: "flex", flexDirection: "column"}}>
+                            <p className="input-label">Email</p>
+                            <input type="text" className="input-field"></input>
+                        </div>
+                        <div style={{display: "flex", flexDirection: "column"}}>
+                            <p className="input-label">Password</p>
+                            <input type="password" className="input-field"></input>
+                        </div>
+                        <p className="additional-text" style={{marginTop:"10px"}}>
+                            <Link to="/forgot" className="additional-link">Forgot Password?</Link>
+                        </p>
 
-                    <p className="additional-text" style={{marginTop:"100px", marginBottom:"10px"}}>Don't have an account?&nbsp;
-                        <Link to="/register" className="additional-link">Register!</Link>
-                    </p>
-                    <div className="btt-register">Log In</div>
+                        <p className="additional-text" style={{marginTop:"100px", marginBottom:"10px"}}>Don't have an account?&nbsp;
+                            <Link to="/register" className="additional-link">Register!</Link>
+                        </p>
+                        <div className="btt-register">Log In</div>
+                    </div>
                 </div>
             </div>
         </div>
