45120c6089887e2f4f208c945533908e40e69349
Lincoln <Lsha0730@gmail.com>
2022-05-26 01:01:46 -0700

    chore: final commit before merge

--- product-site/src/components/dashboard/Dashboard.js
+++ product-site/src/components/dashboard/Dashboard.js
@@ -10,40 +10,40 @@ import statistics from "../home/images/statistics.png";
 import notes from "../home/images/notes.png";
 
 function Dashboard() {
-    const {currentUser} = useContext(siteContext);
-    const [settings, setSettings] = useState({});
-    const database = getDatabase();
+  const {currentUser} = useContext(siteContext);
+  const [settings, setSettings] = useState({});
+  const database = getDatabase();
 
-    // Firebase: Reading Settings
-    useEffect(() => {
-        onValue(ref(database, `appdata/${currentUser}/livedata/settings`), (snapshot) => {
-            let node = snapshot.val();
-            if (!node) {
-              setSettings({conference: "MUNSuite", committee: "Committee"});
-            } else {
-              setSettings(node);
-            }
-          })
-    }, [])
+  // Firebase: Reading Settings
+  useEffect(() => {
+      onValue(ref(database, `appdata/${currentUser}/livedata/settings`), (snapshot) => {
+          let node = snapshot.val();
+          if (!node) {
+            setSettings({conference: "MUNSuite", committee: "Committee"});
+          } else {
+            setSettings(node);
+          }
+        })
+  }, [])
 
-    return (
-      <div className="centering-container">
-        <div className="dashboard-container">
-            <p className="form-heading">Welcome {settings.conference} {settings.committee}!</p>
-            <p className="form-subheading">Account Type:&nbsp;<p style={{color: "#00A3FF"}}>Premium</p></p>
+  return (
+    <div className="centering-container">
+      <div className="dashboard-container">
+          <p className="form-heading">Welcome {settings.conference} {settings.committee}!</p>
+          <p className="form-subheading">Account Type:&nbsp;<p style={{color: "#00A3FF"}}>Premium</p></p>
 
-            <div className="btt-launch-app">Launch App</div>
+          <div className="btt-launch-app">Launch App</div>
 
-            <div className="mockup-container">
-              <img src={delegations} alt="Delegations UI Image" className="mockup" style={{position: "absolute", top: "100px", left: "1000px"}}/>
-              <img src={form} alt="Form UI Image" className="mockup" style={{position: "absolute", top: "0px", left: "500px", zIndex:"1"}}/>
-              <img src={history} alt="History UI Image" className="mockup" style={{position: "absolute", top: "300px", left: "350px"}}/>
-              <img src={statistics} alt="Statistics UI Image" className="mockup" style={{position: "absolute", top: "150px", left: "200px"}}/>
-              <img src={notes} alt="Notes UI Image" className="mockup" style={{position: "absolute", top: "300px", left: "900px"}}/>
-            </div>
-        </div>
+          <div className="mockup-container">
+            <img src={delegations} alt="Delegations UI Image" className="mockup" style={{position: "absolute", top: "100px", left: "1000px"}}/>
+            <img src={form} alt="Form UI Image" className="mockup" style={{position: "absolute", top: "0px", left: "500px", zIndex:"1"}}/>
+            <img src={history} alt="History UI Image" className="mockup" style={{position: "absolute", top: "300px", left: "350px"}}/>
+            <img src={statistics} alt="Statistics UI Image" className="mockup" style={{position: "absolute", top: "150px", left: "200px"}}/>
+            <img src={notes} alt="Notes UI Image" className="mockup" style={{position: "absolute", top: "300px", left: "900px"}}/>
+          </div>
       </div>
-    )
+    </div>
+  )
 }
 
 export default Dashboard;
