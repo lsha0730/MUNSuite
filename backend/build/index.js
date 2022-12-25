3637e677645a061dceaeb8fce4e344d340db3d71
Lincoln Lee <49529139+lsha0730@users.noreply.github.com>
2022-12-25 12:27:44 -0800

    Delete backend/build directory

--- backend/build/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const express = require("express");
-const { registerRouter } = require("./routes/register.js");
-const app = express();
-app.use(express.json());
-app.listen(5000);
-console.log("App listening at port 5000");
-// Routers
-app.use("/register", registerRouter);
