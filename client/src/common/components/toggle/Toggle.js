357c072d71bf45d709fbe804396f202dc51f7838
Lincoln Lee <49529139+lsha0730@users.noreply.github.com>
2023-11-15 13:39:24 +0000

    add small variant and color props to Toggle.js

--- client/src/common/components/toggle/Toggle.js
+++ client/src/common/components/toggle/Toggle.js
@@ -1,21 +1,36 @@
 import React, { useState, useEffect } from "react";
 import "./Toggle.scoped.css";
-import { getTextWidth } from "../../utils/utils";
+import { classNames, getTextWidth } from "../../utils/utils";
 
-const TOGGLE_OFFSET = 25;
+const COLORS = {
+  green: {
+    dark: "#3cdc27",
+    light: "#cff9c3",
+  },
+  red: {
+    dark: "#ff7070",
+    light: "#ffc6c6",
+  },
+};
 
-const Toggle = ({ value, onValue, label, style, ...other }) => {
-  const [on, setOn] = useState(Boolean(value) || false);
-  const labelRight = label.direction === "right";
+const Toggle = ({ size, color, value, onValue, label, style, ...other }) => {
+  const [state, setState] = useState(Boolean(value) || false);
+  const labelRight = label?.direction === "right";
+  const isSmall = size === "small";
+  const offset = isSmall ? 20 : 25;
+  const labelMargin = isSmall ? 10 : 15;
+  const darkColor = state ? COLORS[color].dark : "#aaaaaa";
+  const lightColor = state ? COLORS[color].light : "#eeeeee";
+  const fontSize = isSmall ? 12 : 16;
 
   useEffect(() => {
-    onValue(on);
-  }, [on]);
+    onValue(state);
+  }, [state]);
 
   return (
     <div
       className="container"
-      onClick={() => setOn(!on)}
+      onClick={() => setState(!state)}
       style={Object.assign(
         {
           flexDirection: labelRight ? "row-reverse" : "row",
@@ -26,24 +41,38 @@ const Toggle = ({ value, onValue, label, style, ...other }) => {
     >
       {label && (
         <p
-          className={`label noselect ${on ? "labelon" : "labeloff"}`}
+          className="label noselect"
           style={Object.assign(
             {
-              width: Math.max(getTextWidth(label.on), getTextWidth(label.off)),
+              width: Math.max(
+                getTextWidth(label.on, fontSize),
+                getTextWidth(label.off, fontSize)
+              ),
+              color: darkColor,
+              fontSize: `${fontSize}px`,
             },
             labelRight
-              ? { marginLeft: 15, textAlign: "left" }
-              : { marginRight: 15, textAlign: "right" }
+              ? { marginLeft: labelMargin, textAlign: "left" }
+              : { marginRight: labelMargin, textAlign: "right" }
           )}
         >
-          {on ? label.on : label.off}
+          {state ? label.on : label.off}
         </p>
       )}
 
-      <div className={`bar ${on ? "bgon" : "bgoff"}`}>
+      <div
+        className={classNames("transition200", isSmall ? "bar-sm" : "bar-lg")}
+        style={{ backgroundColor: lightColor }}
+      >
         <div
-          className={`circle ${on ? "btton" : "bttoff"}`}
-          style={{ left: on ? TOGGLE_OFFSET : 0 }}
+          className={classNames(
+            "transition200",
+            isSmall ? "circle-sm" : "circle-lg"
+          )}
+          style={{
+            left: state ? offset : 0,
+            backgroundColor: darkColor,
+          }}
         />
       </div>
     </div>
