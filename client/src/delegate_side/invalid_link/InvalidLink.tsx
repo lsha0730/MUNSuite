df98f80a9f3ec7e717a94b9651ae06b417adf622
Lincoln Lee <lsha0730@gmail.com>
2023-12-21 10:57:20 +0000

    refactor InvalidLink.js

--- client/src/delegate_side/invalid_link/InvalidLink.tsx
+++ client/src/delegate_side/invalid_link/InvalidLink.tsx
@@ -1,15 +1,14 @@
-import React from "react";
 import "./InvalidLink.scoped.css";
 import { BiCompass } from "react-icons/bi";
 
-function InvalidLink() {
+const InvalidLink = () => {
   return (
-    <div className="invalid-container">
+    <div className="container">
       <BiCompass size={90} className="icon" />
       <p className="heading">Invalid Form Link</p>
       <p className="subheading">Contact your staff member</p>
     </div>
   );
-}
+};
 
 export default InvalidLink;
