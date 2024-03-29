cc12de4d46b0bc6827eb1851573dc2d0cc72c794
Lincoln Lee <49529139+lsha0730@users.noreply.github.com>
2024-03-29 13:13:45 +0000

    fix: uneditable individual notes

--- client/src/staff_side/notes/individual_note/IndividualNote.js
+++ client/src/staff_side/notes/individual_note/IndividualNote.js
@@ -1,16 +1,23 @@
+import { useEffect, useState } from "react";
 import "../Notes.scoped.css";
 import "./IndividualNotes.scoped.css";
 
 const IndividualNote = ({ delegate, text, updateNotes }) => {
+  const [note, setNote] = useState(text);
+
+  useEffect(() => {
+    updateNotes(delegate, note);
+  }, [note]);
+
   return (
     <div className="container">
       <p className="title">{delegate}</p>
       <textarea
         type="text"
-        value={text}
+        value={note}
         placeholder="Input here..."
         className="textfield"
-        onChange={(e) => updateNotes(delegate, e.target.value)}
+        onChange={(e) => setNote(e.target.value)}
       />
     </div>
   );
