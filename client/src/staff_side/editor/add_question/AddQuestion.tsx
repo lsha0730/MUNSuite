1f77d21ea159d57e80536eb875a85fbfbc5ec947
Lincoln Lee <49529139+lsha0730@users.noreply.github.com>
2024-04-10 04:08:25 +0000

    refactor: decouple data and restyle

--- client/src/staff_side/editor/add_question/AddQuestion.tsx
+++ client/src/staff_side/editor/add_question/AddQuestion.tsx
@@ -1,32 +1,61 @@
+import { FaHeading } from "react-icons/fa";
 import Button from "../../../common/components/input/Button";
 import { DEFAULT_QTYPE_LABELS } from "../../../common/constants";
-import { QuestionTypes } from "../../../common/types/questionTypes";
+import { QuestionTypes as QT } from "../../../common/types/questionTypes";
 import "./AddQuestion.scoped.css";
+import { BsImages, BsTextLeft } from "react-icons/bs";
+import {
+  MdArrowDropDownCircle,
+  MdCheckBox,
+  MdOutlineRadioButtonChecked,
+  MdShortText,
+} from "react-icons/md";
+import { BiSolidAddToQueue } from "react-icons/bi";
 
 type Props = {
-  addNewBlock: (key: QuestionTypes) => void;
+  addNewBlock: (key: QT) => void;
+};
+
+const QTYPE_ICONS = {
+  [QT.ShortText]: MdShortText,
+  [QT.LongText]: BsTextLeft,
+  [QT.Radio]: MdOutlineRadioButtonChecked,
+  [QT.MultipleChoice]: MdCheckBox,
+  [QT.SelectMultiple]: BiSolidAddToQueue,
+  [QT.Dropdown]: MdArrowDropDownCircle,
+  [QT.Content]: BsImages,
+  [QT.Header]: FaHeading,
 };
 
 function AddQuestion({ addNewBlock }: Props) {
-  const options = Object.values(QuestionTypes);
+  const options = Object.values(QT);
 
   return (
     <div className="container">
-      <p className="text">Add Block</p>
+      <p className="text">Add New</p>
       <div className="options-container">
-        {options.map((qt: QuestionTypes) => (
-          <Button
+        {options.map((qt: QT) => (
+          <AddNewButton
+            qt={qt}
             onClick={() => {
               addNewBlock(qt);
             }}
-            type="light"
-            padding="sm"
-          >
-            {DEFAULT_QTYPE_LABELS[qt]}
-          </Button>
+          />
         ))}
       </div>
     </div>
+  );
+}
+
+function AddNewButton({ qt, onClick }: { qt: QT; onClick: () => void }) {
+  const Icon = QTYPE_ICONS[qt];
+  return (
+    <Button onClick={onClick} type="light" padding="sm">
+      <div className="button_icon_pair">
+        <Icon />
+        {DEFAULT_QTYPE_LABELS[qt]}
+      </div>
+    </Button>
   );
 }
 
