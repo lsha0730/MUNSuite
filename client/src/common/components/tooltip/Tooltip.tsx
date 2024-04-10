29f7feb2621fc8a82b773bc31548c76fc08e1891
Lincoln Lee <49529139+lsha0730@users.noreply.github.com>
2024-04-10 11:19:26 +0000

    feat: make Tooltip.tsx

---
+++ client/src/common/components/tooltip/Tooltip.tsx
@@ -0,0 +1,35 @@
+import { BsFillQuestionCircleFill } from "react-icons/bs";
+import "./Tooltip.scoped.css";
+import Hoverable from "../hover/Hover";
+import { getTextWidth } from "../../utils/utils";
+import { useState } from "react";
+
+type Props = {
+  message: string;
+  direction: "top" | "bottom" | "left" | "right";
+  maxWidth?: number;
+};
+
+const GAP = 10;
+
+const Tooltip = ({ message, direction, maxWidth }: Props) => {
+  const messageWidth = maxWidth || getTextWidth(message, 12);
+  const positions = {
+    top: { top: -GAP },
+    bottom: { bottom: -GAP },
+    left: { left: -(messageWidth + GAP) },
+    right: { right: -(messageWidth + GAP) },
+  };
+
+  return (
+    <Hoverable
+      message={message}
+      pos={positions[direction]}
+      messageStyle={{ maxWidth, textWrap: "balance" }}
+    >
+      <BsFillQuestionCircleFill className="icon" />
+    </Hoverable>
+  );
+};
+
+export default Tooltip;
