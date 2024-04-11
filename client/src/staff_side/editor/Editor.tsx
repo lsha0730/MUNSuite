c84a88103a96a74a013b7c51980ec935ecd10282
Lincoln Lee <49529139+lsha0730@users.noreply.github.com>
2024-04-11 14:28:20 +0000

    minor: change share btt style

--- client/src/staff_side/editor/Editor.tsx
+++ client/src/staff_side/editor/Editor.tsx
@@ -115,7 +115,7 @@ function Editor() {
     <div className="editor-container">
       <div className="main-UI">
         <div className="link-container">
-          <Button type="light" size="md" padding="sm" onClick={copyLink}>
+          <Button type="light" size="md" onClick={copyLink}>
             <BiLink />
             <p style={{ marginLeft: 5 }}>Share</p>
           </Button>
